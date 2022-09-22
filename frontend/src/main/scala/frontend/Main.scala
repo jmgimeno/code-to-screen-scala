1c27a35a959afcb4bb7b97ab2e2a134acc6cfa57
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2022-09-22 09:40:03 +0200

    linked to bootstrap 5.2

--- frontend/src/main/scala/frontend/Main.scala
+++ frontend/src/main/scala/frontend/Main.scala
@@ -3,20 +3,31 @@ package frontend
 import org.scalajs.dom
 import com.raquo.laminar.api.L.*
 
-@main def MainPage(): Unit = 
+@main def MainPage(): Unit =
   val nameVar = Var(initial = "world")
-  val rootElement = div(
-    label("Your name: "),
-    input(
-      onMountFocus,
-      placeholder := "Enter your name here",
-      onInput.mapToValue --> nameVar
-    ),
-    span(
-      "Hello, ",
-      child.text <-- nameVar.signal.map(_.toUpperCase)
+  val rootElement =
+    nav(
+      cls := "navbar navbar-expand-md fixed-top bg-light",
+      div(
+        cls := "container-fluid",
+        a(href := "#", cls := "navbar-brand", "Code to Screen"),
+        div(
+          cls := "collapse navbar-collapse d-flex justify-content-between",
+          idAttr := "navbarCollapse",
+          ul(
+            cls := "navbar-nav",
+            li(cls := "nav-item", a(href := "#", cls := "nav-link active", "Post")),
+            li(cls := "nav-item", a(href := "#", cls := "nav-link", "Show"))
+          ),
+          a(
+            href := "http://github.com",
+            target := "_blank",
+            cls := "d-none d-md-block d-lg-block d-xl-block d-xxl-block",
+            img(src := "./GitHub-Mark-32px.png")
+          )
+        )
+      )
     )
-  )
 
   // In most other examples, containerNode will be set to this behind the scenes
   val containerNode = dom.document.querySelector("#app")
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,2 @@
+addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.6")
 addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.11.0")
