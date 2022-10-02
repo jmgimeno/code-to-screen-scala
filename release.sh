074bfc736600a25db2772272260fb0224bb46918
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2022-10-02 10:00:54 +0200

    add release.sh script

---
+++ release.sh
@@ -0,0 +1,5 @@
+sbtn frontend/fullOptJS
+cp frontend/target/scala-3.2.0/frontend-opt/main.js backend/src/main/resources/public/
+cp frontend/index.html backend/src/main/resources/public/
+cp frontend/public/* backend/src/main/resources/public/
+sbtn backend/assembly
