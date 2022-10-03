c586780faa84109b9a9d6b2b4de562287ad1a7d6
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2022-10-03 15:34:39 +0200

    release.sh used a deprecated command

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-sbtn frontend/fullOptJS
+sbtn frontend/fullLinkJS
 cp frontend/target/scala-3.2.0/frontend-opt/main.js backend/src/main/resources/public/
 cp frontend/index.html backend/src/main/resources/public/
 cp frontend/public/* backend/src/main/resources/public/
